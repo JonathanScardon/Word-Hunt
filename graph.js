e9c99945d86b43cb2c8e2117a5b00f75896e4053
Jonathan Scardon <jxs2372@gmail.com>
2024-08-06 13:50:20 +0800

    Initial Commit

---
+++ graph.js
@@ -0,0 +1,61 @@
+export class Graph {
+    constructor() {
+      this.adjacencyList = {};
+    }
+  
+    addVertex(vertex) {
+      if (!this.adjacencyList[vertex]) {
+        this.adjacencyList[vertex] = [];
+      }
+    }
+  
+    addEdge(vertex1, vertex2) {
+      if (!this.adjacencyList[vertex1]) {
+        this.addVertex(vertex1);
+      }
+      if (!this.adjacencyList[vertex2]) {
+        this.addVertex(vertex2);
+      }
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1); // For undirected graphs
+    }
+  
+    removeEdge(vertex1, vertex2) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+    }
+  
+    removeVertex(vertex) {
+      while (this.adjacencyList[vertex]) {
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacentVertex);
+      }
+      delete this.adjacencyList[vertex];
+    }
+
+    getNeighbors(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            return []
+        }
+        else {
+            return this.adjacencyList[vertex]
+        }
+    }
+
+    getAllNodes() {
+        if (!this.adjacencyList) {
+          return [];
+        }
+        return Object.keys(this.adjacencyList)
+      }
+  
+    printGraph() {
+      for (let vertex in this.adjacencyList) {
+        console.log(vertex + " -> " + this.adjacencyList[vertex].join(", "));
+      }
+    }
+  }
---
+++ processInput.js
@@ -0,0 +1,74 @@
+let mouseDown = false
+let selectedCells = []
+
+/*
+add event listeners to all elements on the grid & background
+*/
+export function initEventListeners() {
+
+    document.querySelectorAll('.grid-element').forEach(cell => {
+    cell.addEventListener('mousedown', startSelection);
+    cell.addEventListener('mouseenter', selectCell);
+})
+    document.addEventListener('mouseup', endSelection)
+}
+
+
+/*
+resets selectedCells to an array of length 0, calls selectCell
+*/
+function startSelection(event){
+    mouseDown = true
+    selectedCells = []
+    selectCell(event)
+}
+
+/*
+If the target cell is a valid selection, pushes it to the list of selected cells
+*/
+function selectCell(event){
+
+    if (mouseDown == true){
+        const cell = event.target;
+        const x = cell.getAttribute('x');
+        const y = cell.getAttribute('y');
+
+        if (isValidSelection(cell, x, y)){
+            cell.classList.add('selected');
+            selectedCells.push({cell, x, y})
+        }
+    }
+}
+
+/*
+when the mouse is released, the selected word is printed and the list of selected cells is reset
+*/
+function endSelection() {    
+    mouseDown = false;
+    console.log('Selected Word:', selectedCells.map(c => c.cell.textContent).join(''));
+    selectedCells = [];
+    document.querySelectorAll('.grid-element.selected').forEach(cell => {
+        cell.classList.remove('selected');
+    });
+    
+}
+
+/*
+returns if a cell is a valid selection
+*/
+function isValidSelection(cell, x, y){
+    //valid case 1: cell is the first element to be selected
+    if (selectedCells.length == 0){
+        return true;
+    }
+
+    const prevCell = selectedCells[selectedCells.length - 1];
+    const deltX = Math.abs(prevCell.x - x);
+    const deltY = Math.abs(prevCell.y - y);
+
+    //valid case 2: cell is adjacent to the last selected cell AND cell has NOT already been selected
+    if ((deltX == 0 && deltY == 1) || (deltX == 1 && deltY == 0) || (deltX == 1 && deltY == 1)){
+        return !selectedCells.some(c => c.x === x && c.y === y);
+    }
+    return false
+}
