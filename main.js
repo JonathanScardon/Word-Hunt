3f4a72b528a3b0d910c92e643618d3471ed8f40e
Jonathan Scardon <jxs2372@gmail.com>
2024-08-07 20:43:08 +0800

    modified processInput to recieve gameBoardInstance, will be used to check solution set and update stats

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ import { initEventListeners } from './processInput.js';
 
 let gameBoardInstance;
 
+
+//**Rename populateBoard to start game?*/
+//**Add an end game function: called when the timer runs out. will redirect to a ending page with summary stats */
+
 function populateBoard() {
     gameBoardInstance = new GameBoard();
     gameBoardInstance.createBoard();
     gameBoardInstance.parseEnglishDict();
     gameBoardInstance.consoleSolutions();
-    initEventListeners()
+    initEventListeners(gameBoardInstance)
 }
 window.populateBoard = populateBoard
