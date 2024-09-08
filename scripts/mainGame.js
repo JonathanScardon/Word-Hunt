bc725bd181b7d9dcb47ae1357cf6f82fc065168b
Jonathan S <js3294@cornell.edu>
2024-09-08 15:27:55 -0400

    renamed gameplay.css to game.css

--- scripts/mainGame.js
+++ scripts/mainGame.js
@@ -13,25 +13,13 @@ window.onload = function() {
     gameBoardInstance.consoleSolutions();
     userInstance = new User()
     initEventListeners(gameBoardInstance, userInstance)
-    countdown(0, 5)
-}
-/*
-function startGame() {
-    gameBoardInstance = new GameBoard();
-    gameBoardInstance.createBoard();
-    gameBoardInstance.parseEnglishDict();
-    gameBoardInstance.consoleSolutions();
-
-    userInstance = new User()
-    initEventListeners(gameBoardInstance, userInstance)
     //countdown(0, 5)
 }
-*/
+
 function endGame() { 
     localStorage.setItem('gameBoardData', JSON.stringify(gameBoardInstance.toJSON()));
     localStorage.setItem('userData', JSON.stringify(userInstance.toJSON()));
     location.href = 'gameEndpage.html';
 }
 
-//window.startGame = startGame
 window.endGame = endGame
