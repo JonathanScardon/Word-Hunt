35bb8d2719c9f62e08de6d1196de2fe856e6d5e1
Jonathan Scardon <jxs2372@gmail.com>
2024-09-08 14:52:41 -0400

    styled startpage

--- scripts/mainGame.js
+++ scripts/mainGame.js
@@ -3,10 +3,8 @@ import { initEventListeners } from './processInput.js';
 import { User } from './user.js'
 import { countdown } from './timer.js'
 
-
 let gameBoardInstance;
 let userInstance;
-
 
 function startGame() {
     gameBoardInstance = new GameBoard();
@@ -14,33 +12,16 @@ function startGame() {
     gameBoardInstance.parseEnglishDict();
     gameBoardInstance.consoleSolutions();
 
-    console.log("within start game")
-    console.log(gameBoardInstance.solutions)
-
-    //userInstance = new User()
-
-    //initEventListeners(gameBoardInstance, userInstance)
-
-    //countdown(0, 5)
-    endGame()
-    setTimeout(endGame, 1 * 1000);
+    userInstance = new User()
+    initEventListeners(gameBoardInstance, userInstance)
+    countdown(0, 5)
 }
 
-function endGame() {
-    /*
+function endGame() { 
     localStorage.setItem('gameBoardData', JSON.stringify(gameBoardInstance.toJSON()));
     localStorage.setItem('userData', JSON.stringify(userInstance.toJSON()));
-
-    const gameBoardData = JSON.parse(localStorage.getItem('gameBoardData'));
-    console.log(gameBoardData);
-    */
-
-    console.log(gameBoardInstance.solutions)
-
-
-    //location.href = 'gameEndpage.html';
+    location.href = 'gameEndpage.html';
 }
-
 
 window.startGame = startGame
 window.endGame = endGame
